1ef5976cd0e30fbdbb956607ae5f3ed827363a69
sachi ishii <sachii858@gmail.com>
2020-07-29 10:51:19 +0000

    first commit

---
+++ practice02.php
@@ -0,0 +1,43 @@
+<?php
+echo 'hello php!';
+echo "\n";
+$a = 3;
+$b = 7;
+echo $a+$b;
+echo "\n";
+
+echo "\n";
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7];
+echo "\n";
+
+echo "\n";
+$hello = 'Hello,';
+$name = 'sachi';
+$world = "'s world!";
+echo $hello.$name.$world;
+echo "\n";
+
+echo "\n";
+$tech_boost = 'tech';
+$tech_boost .= ' boost';
+echo $tech_boost;
+echo "\n";
+
+echo "\n";
+$y2018_calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+echo $y2018_calendar["December"];
+echo "\n";
